f16d357f5fe07291d2fbca4be553ae3df2e3eefd
renann_r <renann_r@hotmail.com>
2025-06-12 21:29:55 -0300

    feat: add service functions to fetch, update, and delete pokemon by id

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { prisma } from '../lib/prisma';
+import { prisma, PrismaClientKnownRequestError } from '../lib/prisma';
 
 export const PokemonSchema = z.object({
   tipo: z.string(),
@@ -13,12 +13,62 @@ export async function listarPokemons() {
 }
 
 export async function criarPokemon(pokemon: PokemonInput) {
-  const novoPokemon = prisma.pokemon.create({
-    data: {
-      ...pokemon,
-      nivel: 1,
-    },
-  });
+  try {
+    const novoPokemon = prisma.pokemon.create({
+      data: {
+        ...pokemon,
+        nivel: 1,
+      },
+    });
+    return novoPokemon;
+  } catch (error) {
+    console.error(error)
+    throw new Error("Erro ao criar pokemon");
+  }
+}
 
-  return novoPokemon;
+export async function atualizarPokemon(id: number, novoTreinador: string) {
+  try {
+    return await prisma.pokemon.update({
+      where: { id },
+      data: { treinador: novoTreinador },
+    });
+  } catch (error: any) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === 'P2025'
+    ) {
+      throw new Error(`Pokémon com id ${id} não encontrado.`);
+    }
+
+    throw error;
+  }
+}
+
+export async function deletarPokemon(id: number) {
+  try {
+    return await prisma.pokemon.delete({
+      where: { id },
+    });
+  } catch (error: any) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === 'P2025'
+    ) {
+      throw new Error(`Pokémon com id ${id} não encontrado para exclusão.`);
+    }
+    throw error;
+  }
+}
+
+export async function buscarPokemonById(id: number) {
+  try {
+    const pokemon = await prisma.pokemon.findUnique({
+      where: { id },
+    });
+
+    return pokemon;
+  } catch (error) {
+    throw new Error(`Pokémon com id ${id} não encontrado.`);
+  }
 }
