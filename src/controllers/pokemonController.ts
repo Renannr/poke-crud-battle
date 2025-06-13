b34eb6e057a66c8f1acc751c14e7402cdbeb9474
renann_r <renann_r@hotmail.com>
2025-06-12 21:27:48 -0300

    refactor: update pokemon controller with support for update and delete operations

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -1,12 +1,13 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { criarPokemon, listarPokemons, PokemonSchema } from '../services/pokemonService';
+import z from 'zod';
+import { atualizarPokemon, buscarPokemonById, criarPokemon, deletarPokemon, listarPokemons, PokemonSchema } from '../services/pokemonService';
 
-export const listarPokemon = async (
-  req: FastifyRequest,
-  res: FastifyReply
-) => {
-  const lista = await listarPokemons();
-  return res.status(200).send(lista);
+const atualizarSchema = z.object({
+  treinador: z.string(),
+});
+
+type Params = {
+  id: string;
 };
 
 export const criaNovoPokemon = async (
@@ -22,4 +23,66 @@ export const criaNovoPokemon = async (
       .status(400)
       .send({ erro: error.message || 'Erro ao criar novo pokemon' });
   }
+};
+
+export const atualizarPokemonById = async (
+  req: FastifyRequest<{ Params: Params }>,
+  res: FastifyReply
+) => {
+  try {
+    const { treinador } = atualizarSchema.parse(req.body);
+    const id = Number(req.params.id);
+
+    const atualizado = await atualizarPokemon(id, treinador);
+
+    return res.status(204).send();
+  } catch (error: any) {
+    return res
+      .status(400)
+      .send({ erro: error.message || 'Erro ao atualizar o pokémon' });
+  }
+};
+
+export const excluirPokemon = async (
+  req: FastifyRequest<{ Params: Params }>,
+  res: FastifyReply
+) => {
+  try {
+    const id = Number(req.params.id);
+    const excluido = await deletarPokemon(id);
+
+    return res.status(204).send();
+  } catch (error: any) {
+    return res
+      .status(400)
+      .send({ erro: error.message || 'Erro ao excluir o pokémon' });
+  }
+};
+
+export const carregarPokemon = async (
+  req: FastifyRequest<{ Params: { id: string } }>,
+  res: FastifyReply
+) => {
+  try {
+    const id = Number(req.params.id);
+    const pokemon = await buscarPokemonById(id);
+
+    if (!pokemon) {
+      return res.status(404).send({ erro: 'Pokémon não encontrado' });
+    }
+
+    return res.status(200).send(pokemon);
+  } catch (error: any) {
+    return res
+      .status(400)
+      .send({ erro: error.message || 'Erro ao buscar o pokemon' });
+  }
+};
+
+export const listarPokemon = async (
+  req: FastifyRequest,
+  res: FastifyReply
+) => {
+  const lista = await listarPokemons();
+  return res.status(200).send(lista);
 };
